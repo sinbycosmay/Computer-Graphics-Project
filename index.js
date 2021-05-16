9ed1c9305acb65422f0007551a06af5e8c61a91a
sohamk990 <sohamk990@gmail.com>
2021-05-16 22:15:46 +0530

    CG

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
 import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
+
 import {FBXLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/FBXLoader.js';
 import {GLTFLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js';
+
+
 
 //SCENE
 let scene = new THREE.Scene();
 scene.fog = new THREE.Fog('grey', 0, 1e4);
+var clock = new THREE.Clock();
+var t = 0;
 
 //RENDER
 let renderer = new THREE.WebGLRenderer({antialias: true});
@@ -14,7 +19,7 @@ renderer.setSize(window.innerWidth,window.innerHeight);
 renderer.setPixelRatio( window.devicePixelRatio);
 renderer.shadowMap.enabled = true;
 // renderer.gammaFactor = 1;
-// renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.outputEncoding = THREE.sRGBEncoding;
 document.body.appendChild(renderer.domElement);
 
 //CAMERA
@@ -42,90 +47,154 @@ road_texture.anisotropy = 32
 road_texture.wrapS = THREE.RepeatWrapping;
 road_texture.wrapT = THREE.RepeatWrapping;
 road_texture.repeat.set(100,100);
-const road_material = new THREE.MeshLambertMaterial({map:road_texture});
+const road_material = new THREE.MeshStandardMaterial({map:road_texture, metalness: 0});
 const road_geometry = new THREE.PlaneBufferGeometry(1e5,1e5,1e3,1e3)
 const plane = new THREE.Mesh(road_geometry,road_material);
 plane.position.x=0;
-plane.position.y=0;
+plane.position.y=-0.01;
 plane.position.z=0;
 plane.castShadow = true;
 plane.receiveShadow = true;
-plane.rotation.x = Math.PI / 2;
+plane.rotation.x = -1*Math.PI / 2;
 plane.doubleSided = true;
 plane.material.side = THREE.DoubleSide;
 scene.add(plane);
 
-//BOX1
-var box_texture = new THREE.TextureLoader().load('/resources/building/123.jpeg');
-box_texture.anisotropy = 32
-box_texture.wrapS = THREE.RepeatWrapping;
-box_texture.wrapT = THREE.RepeatWrapping;
-box_texture.repeat.set(1,1);
-var Height = 50;
-var Breadth = 10;
-var Length = 10;
-var Box_geometry = new THREE.BoxGeometry(Length,Height,Breadth);
-var Box_Material = new THREE.MeshLambertMaterial({map:box_texture});
-var Box_Mesh = new THREE.Mesh(Box_geometry, Box_Material);
-Box_Mesh.position.x = 0;
-Box_Mesh.position.y = Height/2+1;
-Box_Mesh.position.z = 0;
-scene.add(Box_Mesh);
 
-//BOX2
-var Height = 50;
-var Breadth = 10;
-var Length = 10;
-var Box_geometry = new THREE.BoxGeometry(Length,Height,Breadth);
-var Box_Material = new THREE.MeshLambertMaterial({color: 'red'});
-var Box_Mesh = new THREE.Mesh(Box_geometry, Box_Material);
-Box_Mesh.position.x = 250;
-Box_Mesh.position.y = Height/2+1;
-Box_Mesh.position.z = 250;
-scene.add(Box_Mesh);
+// //BOX1
+// var box_texture = new THREE.TextureLoader().load('/resources/building/123.jpeg');
+// box_texture.anisotropy = 32
+// box_texture.wrapS = THREE.RepeatWrapping;
+// box_texture.wrapT = THREE.RepeatWrapping;
+// box_texture.repeat.set(1,1);
+// var Height = 50;
+// var Breadth = 10;
+// var Length = 10;
+// var Box_geometry = new THREE.BoxGeometry(Length,Height,Breadth);
+// var Box_Material = new THREE.MeshLambertMaterial({map:box_texture});
+// var Box_Mesh = new THREE.Mesh(Box_geometry, Box_Material);
+// Box_Mesh.position.x = 0;
+// Box_Mesh.position.y = Height/2+1;
+// Box_Mesh.position.z = 0;
+// scene.add(Box_Mesh);
 
-//BOX3
-var Height = 50;
-var Breadth = 5;
-var Length = 5;
-var Box_geometry = new THREE.BoxGeometry(Length,Height,Breadth);
-var Box_Material = new THREE.MeshLambertMaterial({color: 'green'});
-var Box_Mesh = new THREE.Mesh(Box_geometry, Box_Material);
-Box_Mesh.position.x = -250;
-Box_Mesh.position.y = Height/2+1;
-Box_Mesh.position.z = 250;
-scene.add(Box_Mesh);
+// //BOX2
+// var Height = 50;
+// var Breadth = 10;
+// var Length = 10;
+// var Box_geometry = new THREE.BoxGeometry(Length,Height,Breadth);
+// var Box_Material = new THREE.MeshLambertMaterial({color: 'red'});
+// var Box_Mesh = new THREE.Mesh(Box_geometry, Box_Material);
+// Box_Mesh.position.x = 250;
+// Box_Mesh.position.y = Height/2+1;
+// Box_Mesh.position.z = 250;
+// scene.add(Box_Mesh);
 
-//BOX4
-var Height = 50;
-var Breadth = 5;
-var Length = 5;
-var Box_geometry = new THREE.BoxGeometry(Length,Height,Breadth);
-var Box_Material = new THREE.MeshLambertMaterial({color: 'blue'});
-var Box_Mesh = new THREE.Mesh(Box_geometry, Box_Material);
-Box_Mesh.position.x = 250;
-Box_Mesh.position.y = Height/2+1;
-Box_Mesh.position.z = -250;
-scene.add(Box_Mesh);
+// //BOX3
+// var Height = 50;
+// var Breadth = 5;
+// var Length = 5;
+// var Box_geometry = new THREE.BoxGeometry(Length,Height,Breadth);
+// var Box_Material = new THREE.MeshLambertMaterial({color: 'green'});
+// var Box_Mesh = new THREE.Mesh(Box_geometry, Box_Material);
+// Box_Mesh.position.x = -250;
+// Box_Mesh.position.y = Height/2+1;
+// Box_Mesh.position.z = 250;
+// scene.add(Box_Mesh);
+
+// //BOX4
+// var Height = 50;
+// var Breadth = 5;
+// var Length = 5;
+// var Box_geometry = new THREE.BoxGeometry(Length,Height,Breadth);
+// var Box_Material = new THREE.MeshLambertMaterial({color: 'blue'});
+// var Box_Mesh = new THREE.Mesh(Box_geometry, Box_Material);
+// Box_Mesh.position.x = 250;
+// Box_Mesh.position.y = Height/2+1;
+// Box_Mesh.position.z = -250;
+// scene.add(Box_Mesh);
 
 
-//BOX5
-var Height = 50;
-var Breadth = 5;
-var Length = 5;
-var Box_geometry = new THREE.BoxGeometry(Length,Height,Breadth);
-var Box_Material = new THREE.MeshLambertMaterial({color: 'orange'});
-var Box_Mesh = new THREE.Mesh(Box_geometry, Box_Material);
-Box_Mesh.position.x = -250;
-Box_Mesh.position.y = Height/2+1;
-Box_Mesh.position.z = -250;
-scene.add(Box_Mesh);
+// //BOX5
+// var Height = 50;
+// var Breadth = 5;
+// var Length = 5;
+// var Box_geometry = new THREE.BoxGeometry(Length,Height,Breadth);
+// var Box_Material = new THREE.MeshLambertMaterial({color: 'orange'});
+// var Box_Mesh = new THREE.Mesh(Box_geometry, Box_Material);
+// Box_Mesh.position.x = -250;
+// Box_Mesh.position.y = Height/2+1;
+// Box_Mesh.position.z = -250;
+// scene.add(Box_Mesh);
 
+
+// path 
+var curveObj = new THREE.CatmullRomCurve3([
+    new THREE.Vector3(0, 0, -10),
+    new THREE.Vector3(-10, 0, 0),
+    new THREE.Vector3(-5, 0, 10),
+    new THREE.Vector3(-2.5, 0, 5),
+    new THREE.Vector3(0, 0, 10),
+    new THREE.Vector3(5, 0, 12),
+    new THREE.Vector3(7, 0, -7)
+  ]);
+  curveObj.closed = true;
+  var curveGeom = new THREE.BufferGeometry().setFromPoints(curveObj.getPoints(200));
+  var curveMat = new THREE.LineBasicMaterial({color: 0x444400});
+  var curve = new THREE.Line(curveGeom, curveMat);
+  scene.add(curve);
+
+
+// light holder
+var carProfileShape = new THREE.Shape([
+    new THREE.Vector2(1, 0),
+    new THREE.Vector2(1, 0.25),
+    new THREE.Vector2(0.65, 0.25),
+    new THREE.Vector2(0.35, 0.5),
+    new THREE.Vector2(-0.25, 0.5),
+    new THREE.Vector2(-0.95, 0.25),
+    new THREE.Vector2(-1, 0.25),
+    new THREE.Vector2(-1, 0)
+  ]);
+  var carProfileGeometry = new THREE.ExtrudeBufferGeometry(carProfileShape, {depth: 1, bevelEnabled: false});
+  carProfileGeometry.translate(0, 0, -0.5);
+  carProfileGeometry.rotateY(-Math.PI * 0.5);
+  var lightHolder = new THREE.Mesh(carProfileGeometry, new THREE.MeshLambertMaterial({color: "magenta"}));
+  scene.add(lightHolder);
+  
+  // lights
+  createLight(lightHolder, -0.3);
+  createLight(lightHolder, 0.3);
+  
+  function createLight(base, shift) 
+  {
+    let bulb = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshBasicMaterial());
+    bulb.scale.setScalar(0.1);
+    bulb.position.set(shift, 0.125, 1);
+    base.add(bulb);
+  
+    let light = new THREE.SpotLight('red', 50, 10, THREE.Math.degToRad(60), 0.25);
+    light.position.set(shift, 0.125, 1);
+    base.add(light);
+    
+    let lightTarget = new THREE.Object3D();
+    lightTarget.position.set(shift, 0.125, 1 + 0.1);
+    base.add(lightTarget);
+    light.target = lightTarget;
+  }
+  
 
 //RENDERING
 var render = function()
 {
+    t = clock.getElapsedTime() * 0.1;
+  
+    lightHolder.position.copy(curveObj.getPointAt( t % 1));
+    lightHolder.lookAt(curveObj.getPointAt((t + 0.01) % 1));
     requestAnimationFrame(render);
     renderer.render(scene, camera);
+    
 };
 render();
+
+
--- temp.js
+++ temp.js
@@ -1,75 +1,110 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
+import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
+
 var scene = new THREE.Scene();
-
-var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
-camera.position.z = 5;
-
+var camera = new THREE.PerspectiveCamera(60, 1, 1, 1000);
+camera.position.set(0, 27, 5);
 var renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setClearColor("#e5e5e5");
-renderer.setSize(window.innerWidth,window.innerHeight);
+var canvas = renderer.domElement
+document.body.appendChild(canvas);
 
-document.body.appendChild(renderer.domElement);
+var controls = new OrbitControls(camera, canvas);
 
-window.addEventListener('resize', () => {
-    renderer.setSize(window.innerWidth,window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
+scene.add(new THREE.AmbientLight(0xffffff, 0.125));
 
-    camera.updateProjectionMatrix();
-})
+var dirLight = new THREE.DirectionalLight(0xffffff, 0.05);
+dirLight.position.setScalar(10);
+scene.add(dirLight);
 
-var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2();
+// path 
+var curveObj = new THREE.CatmullRomCurve3([
+  new THREE.Vector3(0, 0, -10),
+  new THREE.Vector3(-10, 0, 0),
+  new THREE.Vector3(-5, 0, 10),
+  new THREE.Vector3(-2.5, 0, 5),
+  new THREE.Vector3(0, 0, 10),
+  new THREE.Vector3(5, 0, 12),
+  new THREE.Vector3(7, 0, -7)
+]);
+curveObj.closed = true;
+var curveGeom = new THREE.BufferGeometry().setFromPoints(curveObj.getPoints(200));
+var curveMat = new THREE.LineBasicMaterial({color: 0x444400});
+var curve = new THREE.Line(curveGeom, curveMat);
+scene.add(curve);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshLambertMaterial({color: 0xF7F7F7});
-//var mesh = new THREE.Mesh(geometry, material);
+// ground
+var ground = new THREE.Mesh(new THREE.PlaneBufferGeometry(30, 30), new THREE.MeshStandardMaterial({map: new THREE.TextureLoader().load("https://threejs.org/examples/textures/hardwood2_diffuse.jpg"), metalness: 0, roughtness: 1}));
+ground.geometry.rotateX(-Math.PI * 0.5);
+ground.position.set(0, -0.01, 0);
+scene.add(ground);
 
-//scene.add(mesh);
+// light holder
+var carProfileShape = new THREE.Shape([
+  new THREE.Vector2(1, 0),
+  new THREE.Vector2(1, 0.25),
+  new THREE.Vector2(0.65, 0.25),
+  new THREE.Vector2(0.35, 0.5),
+  new THREE.Vector2(-0.25, 0.5),
+  new THREE.Vector2(-0.95, 0.25),
+  new THREE.Vector2(-1, 0.25),
+  new THREE.Vector2(-1, 0)
+]);
+var carProfileGeometry = new THREE.ExtrudeBufferGeometry(carProfileShape, {depth: 1, bevelEnabled: false});
+carProfileGeometry.translate(0, 0, -0.5);
+carProfileGeometry.rotateY(-Math.PI * 0.5);
+var lightHolder = new THREE.Mesh(carProfileGeometry, new THREE.MeshLambertMaterial({color: "magenta"}));
+scene.add(lightHolder);
 
-meshX = -10;
-for(var i = 0; i<15;i++) {
-    var mesh = new THREE.Mesh(geometry, material);
-    mesh.position.x = (Math.random() - 0.5) * 10;
-    mesh.position.y = (Math.random() - 0.5) * 10;
-    mesh.position.z = (Math.random() - 0.5) * 10;
-    scene.add(mesh);
-    meshX+=1;
+// lights
+createLight(lightHolder, -0.3);
+createLight(lightHolder, 0.3);
+
+function createLight(base, shift) 
+{
+  let bulb = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshBasicMaterial());
+  bulb.scale.setScalar(0.1);
+  bulb.position.set(shift, 0.125, 1);
+  base.add(bulb);
+
+  let light = new THREE.SpotLight(0xffffff, 5, 10, THREE.Math.degToRad(10), 0.25);
+  light.position.set(shift, 0.125, 1);
+  base.add(light);
+  
+  let lightTarget = new THREE.Object3D();
+  lightTarget.position.set(shift, 0.125, 1 + 0.1);
+  base.add(lightTarget);
+  light.target = lightTarget;
 }
 
+var clock = new THREE.Clock();
+var t = 0;
 
-var light = new THREE.PointLight(0xFFFFFF, 1, 1000)
-light.position.set(0,0,0);
-scene.add(light);
-
-var light = new THREE.PointLight(0xFFFFFF, 2, 1000)
-light.position.set(0,0,25);
-scene.add(light);
-
-var render = function() {
-    requestAnimationFrame(render);
-
-
-    renderer.render(scene, camera);
-}
-
-function onMouseMove(event) {
-    event.preventDefault();
-
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-    raycaster.setFromCamera(mouse, camera);
-
-    var intersects = raycaster.intersectObjects(scene.children, true);
-    for (var i = 0; i < intersects.length; i++) {
-        this.tl = new TimelineMax();
-        this.tl.to(intersects[i].object.scale, 1, {x: 2, ease: Expo.easeOut})
-        this.tl.to(intersects[i].object.scale, .5, {x: .5, ease: Expo.easeOut})
-        this.tl.to(intersects[i].object.position, .5, {x: 2, ease: Expo.easeOut})
-        this.tl.to(intersects[i].object.rotation, .5, {y: Math.PI*.5, ease: Expo.easeOut}, "=-1.5")
-    }
-}
-
-
-
-window.addEventListener('mousemove', onMouseMove);
 render();
+// renderer.setSize(window.innerWidth,window.innerHeight);
+// renderer.setPixelRatio( window.devicePixelRatio);
+
+function render() {
+  if (resize(renderer)) {
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+  
+  t = clock.getElapsedTime() * 0.1;
+  
+  lightHolder.position.copy(curveObj.getPointAt( t % 1));
+  lightHolder.lookAt(curveObj.getPointAt((t + 0.01) % 1));
+  
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+function resize(renderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
